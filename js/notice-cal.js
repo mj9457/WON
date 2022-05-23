4ee5d008c78d4bf2f512144f2faa7010f38efddc
mj9457 <mj9457@naver.com>
2022-05-23 10:45:30 +0900

    add notice-calander in notice.html

---
+++ js/notice-cal.js
@@ -0,0 +1,152 @@
+const cal2203 = document.getElementById("notice-cal-3");
+const cal2204 = document.getElementById("notice-cal-4");
+const cal2205 = document.getElementById("notice-cal-5");
+const cal2206 = document.getElementById("notice-cal-6");
+const cal2207 = document.getElementById("notice-cal-7");
+const cal2208 = document.getElementById("notice-cal-8");
+const cal2209 = document.getElementById("notice-cal-9");
+const cal2210 = document.getElementById("notice-cal-10");
+const cal2211 = document.getElementById("notice-cal-11");
+const cal2212 = document.getElementById("notice-cal-12");
+const cal2301 = document.getElementById("notice-cal-1");
+const cal2302 = document.getElementById("notice-cal-2");
+
+const preBtn03 = document.getElementById("cal-pre-btn-3");
+const preBtn04 = document.getElementById("cal-pre-btn-4");
+const preBtn05 = document.getElementById("cal-pre-btn-5");
+const preBtn06 = document.getElementById("cal-pre-btn-6");
+const preBtn07 = document.getElementById("cal-pre-btn-7");
+const preBtn08 = document.getElementById("cal-pre-btn-8");
+const preBtn09 = document.getElementById("cal-pre-btn-9");
+const preBtn10 = document.getElementById("cal-pre-btn-10");
+const preBtn11 = document.getElementById("cal-pre-btn-11");
+const preBtn12 = document.getElementById("cal-pre-btn-12");
+const preBtn01 = document.getElementById("cal-pre-btn-1");
+const preBtn02 = document.getElementById("cal-pre-btn-2");
+
+const nextBtn03 = document.getElementById("cal-next-btn-3");
+const nextBtn04 = document.getElementById("cal-next-btn-4");
+const nextBtn05 = document.getElementById("cal-next-btn-5");
+const nextBtn06 = document.getElementById("cal-next-btn-6");
+const nextBtn07 = document.getElementById("cal-next-btn-7");
+const nextBtn08 = document.getElementById("cal-next-btn-8");
+const nextBtn09 = document.getElementById("cal-next-btn-9");
+const nextBtn10 = document.getElementById("cal-next-btn-10");
+const nextBtn11 = document.getElementById("cal-next-btn-11");
+const nextBtn12 = document.getElementById("cal-next-btn-12");
+const nextBtn01 = document.getElementById("cal-next-btn-1");
+const nextBtn02 = document.getElementById("cal-next-btn-2");
+
+/* pre */
+
+preBtn04.addEventListener("click", function() {
+    cal2204.style.display = "none";
+    cal2203.style.display = "table";
+})
+
+preBtn05.addEventListener("click", function() {
+    cal2205.style.display = "none";
+    cal2204.style.display = "table";
+})
+
+preBtn06.addEventListener("click", function() {
+    cal2206.style.display = "none";
+    cal2205.style.display = "table";
+})
+
+preBtn07.addEventListener("click", function() {
+    cal2207.style.display = "none";
+    cal2206.style.display = "table";
+})
+
+preBtn08.addEventListener("click", function() {
+    cal2208.style.display = "none";
+    cal2207.style.display = "table";
+})
+
+preBtn09.addEventListener("click", function() {
+    cal2209.style.display = "none";
+    cal2208.style.display = "table";
+})
+
+preBtn10.addEventListener("click", function() {
+    cal2210.style.display = "none";
+    cal2209.style.display = "table";
+})
+
+preBtn11.addEventListener("click", function() {
+    cal2211.style.display = "none";
+    cal2210.style.display = "table";
+})
+
+preBtn12.addEventListener("click", function() {
+    cal2212.style.display = "none";
+    cal2211.style.display = "table";
+})
+
+preBtn01.addEventListener("click", function() {
+    cal2301.style.display = "none";
+    cal2212.style.display = "table";
+})
+
+preBtn02.addEventListener("click", function() {
+    cal2302.style.display = "none";
+    cal2301.style.display = "table";
+})
+
+
+/* next - btn */
+nextBtn03.addEventListener("click", function() {
+    cal2203.style.display = "none";
+    cal2204.style.display = "table";
+})
+
+nextBtn04.addEventListener("click", function() {
+    cal2204.style.display = "none";
+    cal2205.style.display = "table";
+})
+
+nextBtn05.addEventListener("click", function() {
+    cal2205.style.display = "none";
+    cal2206.style.display = "table";
+})
+
+nextBtn06.addEventListener("click", function() {
+    cal2206.style.display = "none";
+    cal2207.style.display = "table";
+})
+
+nextBtn07.addEventListener("click", function() {
+    cal2207.style.display = "none";
+    cal2208.style.display = "table";
+})
+
+nextBtn08.addEventListener("click", function() {
+    cal2208.style.display = "none";
+    cal2209.style.display = "table";
+})
+
+nextBtn09.addEventListener("click", function() {
+    cal2209.style.display = "none";
+    cal2210.style.display = "table";
+})
+
+nextBtn10.addEventListener("click", function() {
+    cal2210.style.display = "none";
+    cal2211.style.display = "table";
+})
+
+nextBtn11.addEventListener("click", function() {
+    cal2211.style.display = "none";
+    cal2212.style.display = "table";
+})
+
+nextBtn12.addEventListener("click", function() {
+    cal2212.style.display = "none";
+    cal2301.style.display = "table";
+})
+
+nextBtn01.addEventListener("click", function() {
+    cal2301.style.display = "none";
+    cal2302.style.display = "table";
+})
