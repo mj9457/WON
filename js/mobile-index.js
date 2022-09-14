f66767dea898467584ed886202e28aa21e938858
mj9457 <mj9457@naver.com>
2022-09-14 13:03:21 +0900

    moblie-main

---
+++ js/mobile-index.js
@@ -0,0 +1,30 @@
+// var windowWidth = window.matchMedia("screen and (max-width: 767px)");
+
+// if (windowWidth.matches) {
+//     //screen and (max-width: 767px)에 해당한다.
+
+//     let count = 0;
+
+//     $(".menubar").click(function() {
+//         count++;
+//         if( count % 2 != 0 ) {
+//             $(".menubar").css('height', '260px');
+//             $(".menubar__content--list").css('display', 'flex');
+//             $(".menubar__content").css('marginTop', '10px');
+//         }else {
+//             $(".menubar").css('height', '60px');
+//             $(".menubar__content--list").css('display', 'none');
+//             $(".menubar__content").css('marginTop', '0');
+//             $(".sub-menubar").css('display','none');
+//         }
+//     })
+
+//     $(".menubar").mouseleave(function() {
+//         // $(".sub-menubar").css('display','none');
+//     })
+    
+// } else {
+//     //screen and (max-width: 767px)에 해당하지 않는다.
+
+// }
+
