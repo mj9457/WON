4d12b17143b404cc3d4f9b954a2382fdc20811aa
MINJUN PARK <mj9457@naer,com>
2022-11-14 20:33:31 +0900

    Chore. index.html add mobile class & validate-moblie.js add mobile

--- js/validate-moblie.js
+++ js/validate-moblie.js
@@ -1,11 +1,17 @@
+const kakao_box = document.querySelector(".kakao-box");
+const mail_box = document.querySelector(".mail-box");
+const mobile = document.querySelector(".mobile");
+
 const isMobile = () => {
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         return true;
     }
     return false;
-};
+};  
 
 if(isMobile() === true) {
-    document.querySelector(".kakao-box").style.display = "none";
-    document.querySelector(".mail-box").style.display = "none";
+    kakao_box.style.display = "none";
+    mail_box.style.display = "none";
+    mobile.style.display = "none";
 }
+
